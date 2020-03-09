40f45e3348b92f032302e50bb9f1c5e5e6e83d55
theprogrammermachine <theprogrammermachine@gmail.com>
2020-03-09 08:48:15 +0330

    test

--- routes/config.js
+++ routes/config.js
@@ -34,6 +34,7 @@ router.post('/edit_config', async function (req, res) {
             }
             let updateRes = await mw.Config.updateOne({instaAccId: instaAccId}, {$set: c});
             console.log(updateRes);
+            ipb.stopInstaAgent(instaAcc.instaAccountId);
             ipb.runInstaAgent(instaAcc.instaAccountId, instaAcc.username, instaAcc.password);
             res.send({status: 'success', message: "Insta account config updated successfully."});
         });
